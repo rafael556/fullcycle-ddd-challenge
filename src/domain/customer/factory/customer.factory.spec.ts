2e0e77fae0f52727297bb156ecfa2489ffd1b8f9
rafael556 <rafael7jf@hotmail.com>
2024-09-24 21:54:32 -0300

    feat: add factories

---
+++ src/domain/customer/factory/customer.factory.spec.ts
@@ -0,0 +1,21 @@
+import Address from "../value-object/address";
+import CustomerFactory from "./customer.factory";
+
+describe("Customer factory unit test", () => {
+  it("should create a customer", () => {
+    let customer = CustomerFactory.create("John");
+
+    expect(customer.id).toBeDefined();
+    expect(customer.name).toBe("John");
+    expect(customer.address).toBeUndefined();
+  });
+
+  it("should create a customer with address", () => {
+    const address = new Address('s1', 1, '123', 'c1');
+    let customer = CustomerFactory.createWithAddress("John", address);
+
+    expect(customer.id).toBeDefined();
+    expect(customer.name).toBe("John");
+    expect(customer.address).toBe(address);
+  });
+});
